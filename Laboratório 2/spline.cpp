58484fe238b19ef871414a7b36025c4939a1bb7d
fiorotticaio <caiofiorotti@gmail.com>
2024-10-29 20:29:52 -0300

    :construction: trying to do lab2 exercise

--- Laboratório 2/spline.cpp
+++ Laboratório 2/spline.cpp
@@ -4,12 +4,12 @@
 #include <GL/glut.h>
 #define TAMANHO_JANELA 500
 
-float size = 5.0;
+float size = 1.0;
 
 //Pontos de controle da Spline
 GLfloat ctrlpoints[4][3] = {
-        { -4.0, -4.0, 0.0}, { -2.0, 4.0, 0.0}, 
-        {2.0, -4.0, 0.0}, {4.0, 4.0, 0.0}};
+        { -0.8, -0.8, 0.0}, { -0.4, 0.8, 0.0}, 
+        {0.4, -0.8, 0.0}, {0.8, 0.8, 0.0}};
 
 void init(void) {
    glClearColor(0.0, 0.0, 0.0, 0.0);
@@ -42,35 +42,53 @@ void display(void) {
    glEnd();
    
    /* Desenha os pontos de controle. */
-   // glPointSize(5.0);
-   // glColor3f(1.0, 1.0, 0.0);
-   // glBegin(GL_POINTS);
-   //    for (i = 0; i < 4; i++) 
-   //       glVertex3fv(&ctrlpoints[i][0]);
-   // glEnd();
+   glPointSize(5.0);
+   glColor3f(1.0, 1.0, 0.0);
+   glBegin(GL_POINTS);
+      for (i = 0; i < 4; i++) 
+         glVertex3fv(&ctrlpoints[i][0]);
+   glEnd();
    
    glutSwapBuffers();
 }
 
-void reshape(int w, int h) {
-   //Define a porcao visivel da janela
-   glViewport(0, 0, (GLsizei) w, (GLsizei) h);
+// void reshape(int w, int h) {
+//    //Define a porcao visivel da janela
+//    glViewport(0, 0, (GLsizei) w, (GLsizei) h);
    
-   //Muda para a matriz de projecao (aulas futuras)
-   glMatrixMode(GL_PROJECTION);
-   glLoadIdentity();
+//    //Muda para a matriz de projecao (aulas futuras)
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
 
-   //Controla o redimensionamento da janela mantendo o aspect ration do objeto
-   if (w <= h)
-      glOrtho(-size, size, -size*(GLfloat)h/(GLfloat)w, 
-               size*(GLfloat)h/(GLfloat)w, -size, size);
-   else
-      glOrtho(-size*(GLfloat)w/(GLfloat)h, 
-               size*(GLfloat)w/(GLfloat)h, -size, size, -size, size);
+//    //Controla o redimensionamento da janela mantendo o aspect ration do objeto
+//    if (w <= h)
+//       glOrtho(-size, size, -size*(GLfloat)h/(GLfloat)w, 
+//                size*(GLfloat)h/(GLfloat)w, -size, size);
+//    else
+//       glOrtho(-size*(GLfloat)w/(GLfloat)h, 
+//                size*(GLfloat)w/(GLfloat)h, -size, size, -size, size);
    
-   //Muda para a matriz de trasformacoes (aulas futuras)
-   glMatrixMode(GL_MODELVIEW);
-   glLoadIdentity();
+//    //Muda para a matriz de trasformacoes (aulas futuras)
+//    glMatrixMode(GL_MODELVIEW);
+//    glLoadIdentity();
+// }
+
+void motion(int x, int y) {
+   //Converte as coordenadas do mouse para o intervalo [-1, 1]
+   GLfloat xf = (2.0*x)/TAMANHO_JANELA - 1.0;
+   GLfloat yf = 1.0 - (2.0*y)/TAMANHO_JANELA;
+   
+   //Atualiza o ponto de controle clicado (dentro de um raio de 30 pixels)
+   for (int i = 0; i < 4; i++) {
+      if (abs(xf - ctrlpoints[i][0]) < 0.1 && abs(yf - ctrlpoints[i][1]) < 0.1) { // 0.1 = 30/TAMANHO_JANELA
+         ctrlpoints[i][0] = xf;
+         ctrlpoints[i][1] = yf;
+         break;
+      }
+   }
+
+   //Indica que a tela deve ser renderizada novamente
+   glutPostRedisplay();
 }
 
 int main(int argc, char** argv) {
@@ -81,7 +99,8 @@ int main(int argc, char** argv) {
    glutCreateWindow (argv[0]);
    init ();
    glutDisplayFunc(display);
-   glutReshapeFunc(reshape);
+   // glutReshapeFunc(reshape);
+   glutMotionFunc(motion);
    glutMainLoop();
    return 0;
 }
