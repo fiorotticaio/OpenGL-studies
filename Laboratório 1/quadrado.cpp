9346bf7f7839cbcc8f98fa03c791ed1fafe0576f
fiorotticaio <caiofiorotti@gmail.com>
2024-10-29 18:53:21 -0300

    :lipstick: changing object pos

--- Laboratório 1/quadrado.cpp
+++ Laboratório 1/quadrado.cpp
@@ -12,10 +12,10 @@ void display(void) {
    glColor3f (1.0, 1.0, 1.0);
    /* Desenhar um polígono branco (retângulo) */
    glBegin(GL_POLYGON);
-      glVertex3f (0.25, 0.25, 0.0);
-      glVertex3f (0.75, 0.25, 0.0);
-      glVertex3f (0.75, 0.75, 0.0);
-      glVertex3f (0.25, 0.75, 0.0);
+      glVertex3f (0.1, 0.1, 0.0);
+      glVertex3f (1, 0.1, 2);
+      glVertex3f (0.5, 0.75, 0.0);
+      glVertex3f (0.1, 0.75, 0.0);
    glEnd();
 
    /* Desenhar no frame buffer! */
